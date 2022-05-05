81e9b5b922c24df66004e5b11fc853cad2d2f26b
kabi239 <kavyasharma239@gmail.com>
2022-05-06 00:02:16 +0530

    added wake word

--- VA1.py
+++ VA1.py
@@ -155,125 +155,130 @@ def destination_language():
     
 if __name__=='__main__':
     print("Loading your AI personal assistant ARA")
-    speak("Loading your AI personal assistant ARA")
-    gretings() 
+    
 
     while True:
-        speak("Tell me how can I help you ?")
-        statement =takeCommand()
-        if statement==0:
-            continue
-        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant is shutting down,Good bye')
-            print('your personal assistant is shutting down,Good bye')
-            break
+        permission=takeCommand()
+        if "ara" in permission:
+            speak("Loading your AI personal assistant ARA")
+            gretings() 
+            speak("Tell me how can I help you ?")
+            statement =takeCommand()
+            if statement==0:
+                continue
+            if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+                speak('your personal assistant is shutting down,Good bye')
+                print('your personal assistant is shutting down,Good bye')
+                break
 
-        if 'wikipedia' in statement:
-            speak('Searching Wikipedia...')
-            statement =statement.replace("wikipedia", "")
-            results = wikipedia.summary(statement, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-            time.sleep(5)
+            if 'wikipedia' in statement:
+                speak('Searching Wikipedia...')
+                statement =statement.replace("wikipedia", "")
+                results = wikipedia.summary(statement, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+                time.sleep(5)
 
-        elif 'open youtube' in statement:
-            webbrowser.open_new_tab("https://www.youtube.com")
-            speak("youtube is open now")
-            time.sleep(5)
+            elif 'open youtube' in statement:
+                webbrowser.open_new_tab("https://www.youtube.com")
+                speak("youtube is open now")
+                time.sleep(5)
 
-        elif 'open google' in statement:
-            webbrowser.open_new_tab("https://www.google.com")
-            speak("Google chrome is open now")
-            time.sleep(5)
+            elif 'open google' in statement:
+                webbrowser.open_new_tab("https://www.google.com")
+                speak("Google chrome is open now")
+                time.sleep(5)
 
-        elif 'open gmail' in statement:
-            webbrowser.open_new_tab("gmail.com")
-            speak("Google Mail open now")
-            time.sleep(5)
+            elif 'open gmail' in statement:
+                webbrowser.open_new_tab("gmail.com")
+                speak("Google Mail open now")
+                time.sleep(5)
+            
+            elif 'open news' in statement:
+                news = webbrowser.open_new_tab(' https://timesofindia.indiatimes.com/home/headlines%E2%80%9D' )
+                speak('Here are some headlines from the Times of India,Happy reading')
+                time.sleep(6)
+
+            elif 'write an email' in statement:
+                try:
+                    speak("What should I say?")
+                    content = takeCommand()
+                    speak("To whome should i send?")
+                    to = takeCommand()    
+                    sendEmail(to, content)
+                    speak("Email has been sent!")
+                except Exception as e:
+                    print(e)
+                    speak("Sorry! your email was not sent")
+
+            elif 'the time' in statement:
+                strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+                speak(f" the time is {strTime}")  
+
+            elif "day it is" in statement:
+                tellDay()
+                continue
+            
+            elif "tell me the time" in statement:
+                tellTime()
+                continue  
+            elif "set an alarm" in statement:
+                speak("What is the alarm in relation to?: ")
+                words=takeCommand()
+                time.sleep(2)
+                speak("For how many seconds: ")
+                sec= takeCommand()
+                time.sleep(1)
+                timer(words,int(sec) )
+            elif "search for me" in statement:
+                speak("what do you want me to search")
+                target=takeCommand()
+                time.sleep(1)
+                kt.search(target)
+            elif "repeat after me" in statement:
+                speak("what would you like me to repeat?")
+                input1=takeCommand()
+                time.sleep(3)
+                speak("you said"+" "+input1)
+                print()
+            elif "what can you do" in statement:
+                speak("I am glad you asked i can do many things ...  like I can tell you time, do search for you , open youTube ,set an alarm and many more")
+                print("I am glad you asked i can do many things ...  like I can tell you time, do search for you , open youTube ,set an alarm and many more")
+            elif "can you translate" in statement:
+                speak("what language should i translate to")
+                to_lang = destination_language()
+                while(True):
+                    while (to_lang not in dic):
+                        print("Language in which you are trying\
+                        to convert is currently not available ,\
+                        please input some other language")
+                        speak("Language in which you are trying\
+                        to convert is currently not available ,\
+                        please input some other language")
+                    #to_lang = destination_language()
         
-        elif 'open news' in statement:
-            news = webbrowser.open_new_tab(' https://timesofindia.indiatimes.com/home/headlines%E2%80%9D' )
-            speak('Here are some headlines from the Times of India,Happy reading')
-            time.sleep(6)
-
-        elif 'write an email' in statement:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                speak("To whome should i send?")
-                to = takeCommand()    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry! your email was not sent")
-
-        elif 'the time' in statement:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f" the time is {strTime}")  
-
-        elif "day it is" in statement:
-            tellDay()
+                    to_lang = dic[dic.index(to_lang)+1]
+        # invoking Translator
+                    translator = Translator()
+                    speak("What would you like to translate")
+                    query=takeCommand()
+        # Translating from src to dest
+                    text_to_translate = translator.translate(query, dest=to_lang)
+                    text = text_to_translate.text
+        # Using Google-Text-to-Speech ie, gTTS() method
+        # to speak the translated text into the
+        # destination language which is stored in to_lang.
+        # Also, we have given 3rd argument as False because
+        # by default it speaks very slowly
+                    speak = gTTS(text=text, lang=to_lang, slow=False)
+        # Using save() method to save the translated
+        # speech in capture_voice.mp3
+                    speak.save("captured_voice.mp3")
+        # Using OS module to run the translated voice.
+                    playsound('captured_voice.mp3')
+                    os.remove('captured_voice.mp3')
+        # Printing Output
+                    print(text) 
             continue
-          
-        elif "tell me the time" in statement:
-            tellTime()
-            continue  
-        elif "set an alarm" in statement:
-            speak("What is the alarm in relation to?: ")
-            words=takeCommand()
-            time.sleep(2)
-            speak("For how many seconds: ")
-            sec= takeCommand()
-            time.sleep(1)
-            timer(words,int(sec) )
-        elif "search for me" in statement:
-            speak("what do you want me to search")
-            target=takeCommand()
-            time.sleep(1)
-            kt.search(target)
-        elif "repeat after me" in statement:
-            speak("what would you like me to repeat?")
-            input1=takeCommand()
-            time.sleep(3)
-            speak("you said"+" "+input1)
-            print()
-        elif "what can you do" in statement:
-            speak("I am glad you asked i can do many things ...  like I can tell you time, do search for you , open youTube ,set an alarm and many more")
-            print("I am glad you asked i can do many things ...  like I can tell you time, do search for you , open youTube ,set an alarm and many more")
-        elif "can you translate" in statement:
-            speak("what language should i translate to")
-            to_lang = destination_language()
-            while(True):
-                while (to_lang not in dic):
-                    print("Language in which you are trying\
-                    to convert is currently not available ,\
-                    please input some other language")
-                    speak("Language in which you are trying\
-                    to convert is currently not available ,\
-                    please input some other language")
-                #to_lang = destination_language()
-    
-                to_lang = dic[dic.index(to_lang)+1]
-    # invoking Translator
-                translator = Translator()
-                speak("What would you like to translate")
-                query=takeCommand()
-    # Translating from src to dest
-                text_to_translate = translator.translate(query, dest=to_lang)
-                text = text_to_translate.text
-    # Using Google-Text-to-Speech ie, gTTS() method
-    # to speak the translated text into the
-    # destination language which is stored in to_lang.
-    # Also, we have given 3rd argument as False because
-    # by default it speaks very slowly
-                speak = gTTS(text=text, lang=to_lang, slow=False)
-    # Using save() method to save the translated
-    # speech in capture_voice.mp3
-                speak.save("captured_voice.mp3")
-    # Using OS module to run the translated voice.
-                playsound('captured_voice.mp3')
-                os.remove('captured_voice.mp3')
-    # Printing Output
-                print(text)
+        
